2a1cee13ef35fcc8fc7407b6404bcc5e4e6c6883
Sejin Kim <kim3@kenyon.edu>
2021-03-15 23:25:29 -0400

    Add the analysis script and empty other files

---
+++ src/analysis/modimport.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+March Madness Analysis
+Steven Lucas & Sejin Kim
+STAT 306 S21 @ Kenyon College
+
+Module import script. Requires following dependencies.
+"""
+
---
+++ src/analysis/rdsfunctions.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+March Madness Analysis
+Steven Lucas & Sejin Kim
+STAT 306 S21 @ Kenyon College
+
+RDS-specific function definitions
+"""
+
